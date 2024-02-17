52af18f605ffa9f8c82b2310e7d2de2a11f69748
nurmukhamed <nurkamalnurmukhamed@gmail.com>
2024-02-17 17:21:18 +0600

    feat: added AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterUserRequest;
+use App\Jobs\SendVerificationCode;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\VerificationCode;
+use Illuminate\Support\Facades\Cache;
+
+class AuthController extends Controller
+{
+
+    public function register(RegisterUserRequest $request)
+    {
+        $user = new User([
+            'full_name' => $request->full_name,
+            'phone_number' => $request->phone_number,
+        ]);
+
+        $code = rand(10000, 99999);
+
+        dispatch(new SendVerificationCode($user->phone_number, $code));
+
+        Cache::put($user->phone_number, ['user' => $user, 'code' => $code], 600);
+
+        return response()->json([
+            'message' => 'Мы отправили SMS с кодом активации на ваш телефон ' . $user->phone_number
+        ]);
+    }
+
+    public function verify(Request $request)
+    {
+        $data = Cache::get($request->phone_number);
+
+        if (!$data) {
+            return response()->json([
+                'message' => 'Код верификации не найден'
+            ], 404);
+        }
+
+        if ($data['code'] != $request->code) {
+            return response()->json([
+                'message' => 'Код верификации не совпадает'
+            ], 400);
+        }
+
+        $user = $data['user'];
+
+        $user->save();
+
+        Cache::forget($request->phone_number);
+
+        return response()->json([
+            'message' => 'Вы успешно зарегистрированы'
+        ]);
+    }
+
+}
---
+++ app/Jobs/SendVerificationCode.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Mobizon\MobizonApi;
+
+class SendVerificationCode implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $phone_number;
+    protected $code;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(string $phone_number, string $code)
+    {
+        $this->phone_number = $phone_number;
+        $this->code = $code;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $api = new MobizonApi([
+            'apiKey' => env('MOBIZON_API_KEY'),
+            'apiServer' => env('MOBIZON_API_SERVER', 'https://api.mobizon.kz')
+        ]);
+
+        $api->call('message', 'sendSMSMessage', [
+            'recipient' => $this->phone_number,
+            'text' => "Ваш код верификации: {$this->code}"
+        ]);
+    }
+}
